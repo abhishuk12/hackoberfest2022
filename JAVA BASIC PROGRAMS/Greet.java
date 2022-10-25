042a9b2585c3a8df577d4f70f9d9a79e3618845a
anushkag031 <anushkagupta319@gmail.com>
2022-10-25 22:38:41 +0530

    java basic programs

---
+++ JAVA BASIC PROGRAMS/Greet.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Greet {
+    public static void main (String []) args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter you name :");
+        String name=sc.nextLine();
+        System.out.println("hello"+name+"have a good day! ");
+
+    }   
+}
