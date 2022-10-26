1818aa357c1db59b9d9212713ada373961bc3a5a
pahalpriya <priyapahal5@gmail.com>
2022-10-27 00:05:48 +0530

    3rd Commit

---
+++ Project.java
@@ -0,0 +1,10 @@
+public class Project {
+    public static void main (String args[]){
+        Constructor(){
+            //here constructor is created 
+            System.out.println("oject is created sucessfully ");
+        }
+        // now lets create an object  
+    Constructor obj=new Constructor(); //here the object is created     
+    }
+}
