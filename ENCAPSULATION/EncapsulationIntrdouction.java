847a8359cfdda4af37ff5f127e60f1e55544cd1c
garimarajput9 <rajputgarima444@gmail.com>
2022-10-31 00:53:32 +0530

    encapsulation

---
+++ ENCAPSULATION/EncapsulationIntrdouction.java
@@ -0,0 +1,11 @@
+public class EncapsulationIntrdouction {
+  public static void main (String[] args) {
+   
+    Student obj=new Student();
+
+    // obj.name="sunny";
+    // obj.age=23;
+    obj.setAge(34);
+    
+  }   
+}
---
+++ ENCAPSULATION/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+
+    // to achieve encapsulation we first set all the variable of a class to private 
+
+   private  int age ;
+   private String name;
+
+
+    // now lets use public getter abd setter method
+      public int getAge(){
+        return Age;
+      }
+      public void setAge(Age){
+        // now we can set condition here that the user only enter this data 
+        if(Age>20)
+        {
+        System.out.println("you were too old ");
+        }
+        this.age=age;
+      }
+       
+
+    
+}
